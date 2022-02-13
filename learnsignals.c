ad361d3517f8701f63523bf328f03ac326e5454e
Ahmed Jadani <ahjadani@e1r1p1.1337.ma>
2022-02-13 19:12:07 +0100

    sig

--- learnsignals.c
+++ learnsignals.c
@@ -1,16 +1,33 @@
+/* ************************************************************************************************ */
+/*                                                                                                  */
+/*                                                        :::   ::::::::   ::::::::  :::::::::::    */
+/*   learnsignals.c                                    :+:+:  :+:    :+: :+:    :+: :+:     :+:     */
+/*                                                      +:+         +:+        +:+        +:+       */
+/*   By: ahjadani <ahjadani@student.1337.ma>           +#+      +#++:      +#++:        +#+         */
+/*                                                    +#+         +#+        +#+      +#+           */
+/*   Created: 2022/02/13 19:04:37 by ahjadani        #+#  #+#    #+# #+#    #+#     #+#             */
+/*   Updated: 2022/02/13 19:04:37 by ahjadani     ####### ########   ########      ###.ma           */
+/*                                                                                                  */
+/* ************************************************************************************************ */
+
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void handle_sig(int sig)
+void handle_sig(int sig/*, siginfo_t *info , void *other*/)
 {
 	printf("sig: %d\n", sig);
+
 }
 
 int main()
 {
-	
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGTSTP, SIG_IGN);
+	struct sigaction sgn;
+	printf("The PID is: %d\n", getpid());	
+	// sgn.sa_sigaction = handle_sig;
+	// signal(SIGQUIT, SIG_IGN);
+	// signal(SIGTSTP, SIG_IGN);
 	signal(SIGINT, handle_sig);
-	while (1);
+	while (pause());
 }
